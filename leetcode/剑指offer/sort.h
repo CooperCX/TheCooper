a2245353e6673e598d2232837adc8384bc90462f
CooperCX <40228131+CooperCX@users.noreply.github.com>
2023-04-13 10:43:45 +0000

    feat: add shell quick and merge sort

--- leetcode/剑指offer/sort.h
+++ leetcode/剑指offer/sort.h
@@ -50,4 +50,82 @@ public:
             numbers[i + 1] = key;
         }
     }
+
+    /*希尔排序*/
+    void shellSort(std::vector<int>& numbers) {
+        int gap = numbers.size() / 2;  //implicit
+        while (gap >= 1) {
+            for (int i = gap; i < static_cast<int>(numbers.size()); i++) {
+                int j  = i - gap;
+                int key = numbers[i];
+                while (j >= 0 && numbers[j] > key) {
+                    numbers[j + gap] = numbers[j];
+                    j -= gap;
+                }
+                numbers[j + gap] = key;
+            }
+            gap /= 2;
+        }
+    }
+
+    /*快速排序*/
+    void quickSort(std::vector<int>& numbers, int start, int end) {
+        if (start >= end) {
+            return;
+        }
+        int pivot = numbers[start];
+        int first = start, last = end;
+        while (first < last) {
+            while (first < last && numbers[last] > pivot) {
+                last--;
+            }
+            if (first < last) {
+                numbers[first++] = numbers[last];
+            }
+            while (first < last && numbers[first] < pivot) {
+                first++;
+            }
+            if (first < last) {
+                numbers[last--] = numbers[first];
+            }
+        }
+        numbers[first] = pivot;
+        quickSort(numbers, start, first - 1);
+        quickSort(numbers, first + 1, end);
+    }
+
+    /*归并排序*/
+    void mergeSort(std::vector<int>& numbers) {
+        int length = numbers.size(); // implicit
+        std::vector<int> temp(length, 0);
+        mergeSortCore(numbers, temp, 0, length);
+    }
+
+private:
+
+    /*归并排序*/
+    void mergeSortCore(std::vector<int>& numbers, std::vector<int>& temp, int start, int end) {
+        if (start >= end) {
+            return;
+        }
+        int mid = start + (end - start) / 2;
+        int lstart = start, lend = mid, rstart = mid + 1, rend = end;
+        mergeSortCore(numbers, temp, lstart, lend);
+        mergeSortCore(numbers, temp, rstart, rend);
+
+        int index = start;
+        while (lstart <= lend && rstart <= rend) {
+            temp[index++] = numbers[lstart] < numbers[rstart] ? numbers[lstart++] : numbers[rstart++];
+        }
+        while (lstart <= lend) {
+            temp[index++] = numbers[lstart++];
+        }
+        while (rstart <= rend) {
+            temp[index++] = numbers[rstart++];
+        }
+
+        for (int i = start; i <= end; i++) {
+            numbers[i] = temp[i];
+        }
+    }
 };
